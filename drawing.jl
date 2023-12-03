975b937b7d868cca9a5266bc2e4ad7dfc6412ff2
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2023-12-03 11:09:11 -0600

    add drawing.jl (should have been committed earlier)

---
+++ drawing.jl
@@ -0,0 +1,24 @@
+import SimpleDraw as SD
+
+function draw_game!(render_region, game_state)
+    render_region_height, render_region_width = size(render_region)
+
+    SD.draw!(render_region, SD.Background(), 0x00cccccc)
+
+    player = game_state.player
+    player_shape = get_player_shape(player)
+
+    player_shape_wrt_render_region = get_shape_wrt_render_region(game_state.camera, render_region_height, render_region_width, player_shape)
+
+    player_direction_shape_wrt_render_region = get_player_direction_shape_wrt_render_region(game_state, player_shape_wrt_render_region)
+
+    SD.draw!(render_region, player_shape_wrt_render_region, 0x000000ff)
+
+    SD.draw!(render_region, player_direction_shape_wrt_render_region, 0x00000000)
+
+    reference_circle_wrt_render_region = get_shape_wrt_render_region(game_state.camera, render_region_height, render_region_width, game_state.reference_circle)
+
+    SD.draw!(render_region, reference_circle_wrt_render_region, 0x00ff0000)
+
+    return nothing
+end
