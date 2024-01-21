50c7a21a5c66e8d3c346bbaf29a0185b06c5ab97
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2024-01-21 12:48:58 -0600

    clean up loop and move stuff into functions

--- netcode/game_simulation.jl
+++ netcode/game_simulation.jl
@@ -10,6 +10,8 @@ end
 mutable struct GameState
     reference_time::Int
     frame_number::Int
+    target_frame_rate::Int
+    target_ns_per_frame::Int
 end
 
 function get_time(reference_time)
@@ -26,33 +28,43 @@ function get_time(reference_time)
     end
 end
 
+function simulate_update!(game_state, debug_info)
+    update_time_theoretical = 2_000_000
+    push!(debug_info.update_time_theoretical_buffer, update_time_theoretical)
+    update_start_time = get_time(game_state.reference_time)
+    sleep(update_time_theoretical / 1e9)
+    update_end_time = get_time(game_state.reference_time)
+    push!(debug_info.update_time_observed_buffer, update_end_time - update_start_time)
+
+    return nothing
+end
+
+function sleep_to_achieve_target_frame_rate!(game_state, debug_info)
+    sleep_time_theoretical = max(0, game_state.target_ns_per_frame * game_state.frame_number - get_time(game_state.reference_time))
+    push!(debug_info.sleep_time_theoretical_buffer, sleep_time_theoretical)
+
+    sleep_start_time = get_time(game_state.reference_time)
+    sleep(sleep_time_theoretical / 1e9)
+    sleep_end_time = get_time(game_state.reference_time)
+    push!(debug_info.sleep_time_observed_buffer, sleep_end_time - sleep_start_time)
+
+    return nothing
+end
+
 function start()
-    frames_per_second = 60
-    total_frames = frames_per_second * 2
-    ns_per_frame = 1_000_000_000 ÷ frames_per_second
+    target_frame_rate = 60
+    total_frames = target_frame_rate * 2
+    target_ns_per_frame = 1_000_000_000 ÷ target_frame_rate
 
     debug_info = DebugInfo(Int[], Int[], Int[], Int[])
-    game_state = GameState(0, 1)
-
-    game_state.reference_time = time_ns()
+    game_state = GameState(time_ns(), 1, target_frame_rate, target_ns_per_frame)
 
     while game_state.frame_number <= total_frames
         # GLFW.PollEvents()
 
-        update_time_theoretical = 2_000_000
-        push!(debug_info.update_time_theoretical_buffer, update_time_theoretical)
-        update_start_time = get_time(game_state.reference_time)
-        sleep(update_time_theoretical / 1e9)
-        update_end_time = get_time(game_state.reference_time)
-        push!(debug_info.update_time_observed_buffer, update_end_time - update_start_time)
+        simulate_update!(game_state, debug_info)
 
-        sleep_time_theoretical = max(0, ns_per_frame * game_state.frame_number - get_time(game_state.reference_time))
-        push!(debug_info.sleep_time_theoretical_buffer, sleep_time_theoretical)
-
-        sleep_start_time = get_time(game_state.reference_time)
-        sleep(sleep_time_theoretical / 1e9)
-        sleep_end_time = get_time(game_state.reference_time)
-        push!(debug_info.sleep_time_observed_buffer, sleep_end_time - sleep_start_time)
+        sleep_to_achieve_target_frame_rate!(game_state, debug_info)
 
         game_state.frame_number = game_state.frame_number + 1
     end
