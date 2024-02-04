f3b562be0e7a36f89271d140e8e549938027ae7d
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2024-02-04 11:26:59 -0600

    create sandbox game loop for netcode simulation (#18)
    * add project files for netcode / game simulator
    
    * add basic game simulation
    
    * remove unused frame_start_time_buffer
    
    * clean up loop and move stuff into functions
    
    * add DataFrames package in project files
    
    * track frame times and create dataframe of debug info

---
+++ netcode/game_simulation.jl
@@ -0,0 +1,100 @@
+import Statistics
+import DataFrames as DF
+
+struct DebugInfo
+    frame_end_time_buffer::Vector{Int}
+    frame_time_buffer::Vector{Int}
+    update_time_theoretical_buffer::Vector{Int}
+    update_time_observed_buffer::Vector{Int}
+    sleep_time_theoretical_buffer::Vector{Int}
+    sleep_time_observed_buffer::Vector{Int}
+end
+
+mutable struct GameState
+    reference_time::Int
+    frame_number::Int
+    target_frame_rate::Int
+    target_ns_per_frame::Int
+end
+
+function get_time(reference_time)
+    # get time (in units of nanoseconds) since reference_time
+    # places an upper bound on how much time can the program be running until time wraps around giving meaningless values
+    # the conversion to Int will actually throw an error when that happens
+
+    t = time_ns()
+
+    if t >= reference_time
+        return Int(t - reference_time)
+    else
+        return Int(t + (typemax(t) - reference_time))
+    end
+end
+
+function simulate_update!(game_state, debug_info)
+    update_time_theoretical = 2_000_000
+    push!(debug_info.update_time_theoretical_buffer, update_time_theoretical)
+    update_start_time = get_time(game_state.reference_time)
+    sleep(update_time_theoretical / 1e9)
+    update_end_time = get_time(game_state.reference_time)
+    push!(debug_info.update_time_observed_buffer, update_end_time - update_start_time)
+
+    return nothing
+end
+
+function sleep_to_achieve_target_frame_rate!(game_state, debug_info)
+    sleep_time_theoretical = max(0, game_state.target_ns_per_frame * game_state.frame_number - get_time(game_state.reference_time))
+    push!(debug_info.sleep_time_theoretical_buffer, sleep_time_theoretical)
+
+    sleep_start_time = get_time(game_state.reference_time)
+    sleep(sleep_time_theoretical / 1e9)
+    sleep_end_time = get_time(game_state.reference_time)
+    push!(debug_info.sleep_time_observed_buffer, sleep_end_time - sleep_start_time)
+
+    return nothing
+end
+
+function create_df_debug_info(debug_info)
+    return DF.DataFrame(
+        # :frame_end_time_buffer => debug_info.frame_end_time_buffer,
+        :frame_time => debug_info.frame_time_buffer,
+        :update_time_theoretical => debug_info.update_time_theoretical_buffer,
+        :update_time_observed => debug_info.update_time_observed_buffer,
+        :sleep_time_theoretical => debug_info.sleep_time_theoretical_buffer,
+        :sleep_time_observed => debug_info.sleep_time_observed_buffer,
+    )
+end
+
+function start()
+    target_frame_rate = 60
+    total_frames = target_frame_rate * 2
+    target_ns_per_frame = 1_000_000_000 ÷ target_frame_rate
+
+    debug_info = DebugInfo(Int[], Int[], Int[], Int[], Int[], Int[])
+    game_state = GameState(time_ns(), 1, target_frame_rate, target_ns_per_frame)
+
+    while game_state.frame_number <= total_frames
+        # GLFW.PollEvents()
+
+        simulate_update!(game_state, debug_info)
+
+        sleep_to_achieve_target_frame_rate!(game_state, debug_info)
+
+        push!(debug_info.frame_end_time_buffer, get_time(game_state.reference_time))
+        if game_state.frame_number == 1
+            push!(debug_info.frame_time_buffer, first(debug_info.frame_end_time_buffer))
+        else
+            push!(debug_info.frame_time_buffer, debug_info.frame_end_time_buffer[game_state.frame_number] - debug_info.frame_end_time_buffer[game_state.frame_number - 1])
+        end
+
+        game_state.frame_number = game_state.frame_number + 1
+    end
+
+    df_debug_info = create_df_debug_info(debug_info)
+    display(df_debug_info)
+    display(DF.describe(df_debug_info, :min, :max, :mean, :std))
+
+    return nothing
+end
+
+start()
