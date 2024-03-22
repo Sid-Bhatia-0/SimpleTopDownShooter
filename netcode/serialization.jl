9627c16a02bdef4a122299ddffe852a1711d4bd2
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2024-03-22 18:04:58 -0500

    move try_read methods to serialization.jl

--- netcode/serialization.jl
+++ netcode/serialization.jl
@@ -89,3 +89,135 @@ Base.write(io::IO, private_connect_token_associated_data::PrivateConnectTokenAss
 Base.write(io::IO, packet::AbstractPacket) = write_fields(io, packet)
 
 Base.write(io::IO, packet::ConnectTokenPacket) = write_fields_and_padding(io, packet)
+
+function try_read(io::IO, ::Type{NetcodeInetAddr})
+    address_type = read(io, TYPE_OF_ADDRESS_TYPE)
+
+    if address_type == ADDRESS_TYPE_IPV4
+        host = Sockets.IPv4(read(io, TYPE_OF_IPV4_HOST))
+        port = read(io, TYPE_OF_IPV4_PORT)
+    elseif address_type == ADDRESS_TYPE_IPV6
+        host = Sockets.IPv6(read(io, TYPE_OF_IPV6_HOST))
+        port = read(io, TYPE_OF_IPV6_PORT)
+    else
+        return nothing
+    end
+
+    return NetcodeInetAddr(Sockets.InetAddr(host, port))
+end
+
+function try_read(data::Vector{UInt8}, ::Type{ConnectTokenPacket})
+    if length(data) != SIZE_OF_CONNECT_TOKEN_PACKET
+        return nothing
+    end
+
+    io = IOBuffer(data)
+
+    netcode_version_info = read(io, SIZE_OF_NETCODE_VERSION_INFO)
+    if netcode_version_info != NETCODE_VERSION_INFO
+        return nothing
+    end
+
+    protocol_id = read(io, TYPE_OF_PROTOCOL_ID)
+    if protocol_id != PROTOCOL_ID
+        return nothing
+    end
+
+    create_timestamp = read(io, TYPE_OF_TIMESTAMP)
+    expire_timestamp = read(io, TYPE_OF_TIMESTAMP)
+    if expire_timestamp < create_timestamp
+        return nothing
+    end
+
+    nonce = read(io, SIZE_OF_NONCE)
+
+    encrypted_private_connect_token_data = read(io, SIZE_OF_ENCRYPTED_PRIVATE_CONNECT_TOKEN_DATA)
+
+    timeout_seconds = read(io, TYPE_OF_TIMEOUT_SECONDS)
+
+    num_server_addresses = read(io, TYPE_OF_NUM_SERVER_ADDRESSES)
+    if !(1 <= num_server_addresses <= MAX_NUM_SERVER_ADDRESSES)
+        return nothing
+    end
+
+    netcode_addresses = NetcodeInetAddr[]
+
+    for i in 1:num_server_addresses
+        netcode_address = try_read(io, NetcodeInetAddr)
+        if !isnothing(netcode_address)
+            push!(netcode_addresses, netcode_address)
+        else
+            return nothing
+        end
+    end
+
+    client_to_server_key = read(io, SIZE_OF_KEY)
+
+    server_to_client_key = read(io, SIZE_OF_KEY)
+
+    while !eof(io)
+        x = read(io, UInt8)
+        if x != 0
+            return nothing
+        end
+    end
+
+    connect_token_packet = ConnectTokenPacket(
+        netcode_version_info,
+        protocol_id,
+        create_timestamp,
+        expire_timestamp,
+        nonce,
+        encrypted_private_connect_token_data,
+        timeout_seconds,
+        num_server_addresses,
+        netcode_addresses,
+        client_to_server_key,
+        server_to_client_key,
+    )
+
+    return connect_token_packet
+end
+
+function try_read(data::Vector{UInt8}, ::Type{ConnectionRequestPacket})
+    if length(data) != SIZE_OF_CONNECTION_REQUEST_PACKET
+        return nothing
+    end
+
+    io = IOBuffer(data)
+
+    packet_type = read(io, TYPE_OF_PACKET_TYPE)
+    if packet_type != PACKET_TYPE_CONNECTION_REQUEST_PACKET
+        return nothing
+    end
+
+    netcode_version_info = read(io, SIZE_OF_NETCODE_VERSION_INFO)
+    if netcode_version_info != NETCODE_VERSION_INFO
+        return nothing
+    end
+
+    protocol_id = read(io, TYPE_OF_PROTOCOL_ID)
+    if protocol_id != PROTOCOL_ID
+        return nothing
+    end
+
+    expire_timestamp = read(io, TYPE_OF_TIMESTAMP)
+    if expire_timestamp <= time_ns()
+        return nothing
+    end
+
+    nonce = read(io, SIZE_OF_NONCE)
+
+    encrypted_private_connect_token_data = read(io, SIZE_OF_ENCRYPTED_PRIVATE_CONNECT_TOKEN_DATA)
+
+    connection_request_packet = ConnectionRequestPacket(
+        packet_type,
+        netcode_version_info,
+        protocol_id,
+        expire_timestamp,
+        nonce,
+        encrypted_private_connect_token_data,
+    )
+
+    return connection_request_packet
+end
