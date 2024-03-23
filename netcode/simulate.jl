fde199ad6735fd38d0bedc7e4c4acc2ef4c2cd17
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2024-03-23 13:06:03 -0500

    pretty printing (#35)
    * add package GarishPrint to project files
    
    * use GarishPrint to pretty print connect_token_info
    
    * add and use function pprint

--- netcode/simulate.jl
+++ netcode/simulate.jl
@@ -1,5 +1,6 @@
 import Base64
 import DataFrames as DF
+import GarishPrint as GP
 import HTTP
 import Random
 import SHA
@@ -40,6 +41,12 @@ const USER_DATA = DF.DataFrame(username = ["user$(i)" for i in 1:3], salt = ["$(
 
 const CLIENT_USERNAME = "user1"
 const CLIENT_PASSWORD = "password1"
+
+function pprint(x)
+    GP.pprint(x)
+    println()
+    return nothing
+end
 
 function get_time(reference_time)
     # get time (in units of nanoseconds) since reference_time
@@ -110,6 +117,7 @@ function start_game_server(game_server_address, room_size)
 
             if !isnothing(connection_request_packet)
                 @info "Received PACKET_TYPE_CONNECTION_REQUEST_PACKET"
+                pprint(connection_request_packet)
 
                 for i in 1:room_size
                     if !room[i].is_used
@@ -164,6 +172,8 @@ function start_client(auth_server_address, username, password)
     connection_request_packet_length = write(io_connection_request_packet, connection_request_packet)
     @assert connection_request_packet_length == size_of_connection_request_packet
 
+    pprint(connection_request_packet)
+
     Sockets.send(socket, game_server_address.host, game_server_address.port, io_connection_request_packet.data)
 
     return nothing
@@ -187,7 +197,8 @@ function auth_handler(request)
             else
                 if bytes2hex(SHA.sha3_256(hashed_password * USER_DATA[i, :salt])) == USER_DATA[i, :hashed_salted_hashed_password]
                     connect_token_info = ConnectTokenInfo(i)
-                    @info "connect_token_info struct data" connect_token_info.netcode_version_info connect_token_info.protocol_id connect_token_info.create_timestamp connect_token_info.expire_timestamp connect_token_info.nonce connect_token_info.timeout_seconds connect_token_info.client_id connect_token_info.netcode_addresses connect_token_info.client_to_server_key connect_token_info.server_to_client_key connect_token_info.user_data SERVER_SIDE_SHARED_KEY SIZE_OF_HMAC SIZE_OF_ENCRYPTED_PRIVATE_CONNECT_TOKEN_DATA SIZE_OF_CONNECT_TOKEN_PACKET
+
+                    pprint(connect_token_info)
 
                     connect_token_packet = ConnectTokenPacket(connect_token_info)
 
